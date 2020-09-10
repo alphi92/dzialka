43998beffc197a433b6cea3a26a3037d2db2b30c
alphi92 <alphi922@gmail.com>
2020-09-10 19:10:35 +0200

    added contact form

---
+++ src/components/apikeys.js
@@ -0,0 +1,4 @@
+export default {
+    USER_ID :'user_IeTzbnLxYXhXc9PN7C3uC',
+    TEMPLATE_ID:'template_44jj21v'
+}
--- src/components/kontakt.js
+++ src/components/kontakt.js
@@ -1,53 +1,47 @@
 import React, { Component} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import emailjs from 'emailjs-com'
+import apiKeys from './apikeys'
 
 
 class kontakt extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-          name: '',
-          email: '',
-          message: ''
-        }
+
+  sendEmail = e => {
+    e.preventDefault()
+
+    emailjs
+    .sendForm('service_ywfcu7d', apiKeys.template_44jj21v, e.target, apiKeys.user_IeTzbnLxYXhXc9PN7C3uC)
+    .then(
+      result => {
+        console.log(result.text)
+      },
+      error => {
+        console.log(error.text)
       }
+    )
+    }
+
+    
+    
 
       render() {
+        const form = document.querySelector('.form');
+        if (form) {
+          form.addEventListener('submit',this.sendEmail);
+        }
+        
+
         return(
-          <div className="App">
-          <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-           <div className="form-group">
-               <label htmlFor="name">Name</label>
-               <input type="text" className="form-control" value={this.state.name} onChange={this.onNameChange.bind(this)} />
-           </div>
-           <div className="form-group">
-               <label htmlFor="exampleInputEmail1">Email address</label>
-               <input type="email" className="form-control" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
-           </div>
-           <div className="form-group">
-               <label htmlFor="message">Message</label>
-               <textarea className="form-control" rows="5" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
-           </div>
-           <button type="submit" className="btn btn-primary">Submit</button>
-           </form>
-           </div>
+          <form class="form">
+            <input name='name' type="text" placeholder="Your name..." class="form__input" />
+            <input name='topic' type="text" placeholder="Topic..." class="form__input"  />
+            <textarea name='message' type="text" placeholder="Your Message..." class="form__input"  ></textarea>
+            <input type="submit" value="send" class="form__input form__input--button"/>
+          </form>
         );
+        
        }
        
-         onNameChange(event) {
-           this.setState({name: event.target.value})
-         }
-       
-         onEmailChange(event) {
-           this.setState({email: event.target.value})
-         }
-       
-         onMessageChange(event) {
-           this.setState({message: event.target.value})
-         }
-       
-       handleSubmit(event) {
-       }
-       }
+}
        
        export default kontakt;
